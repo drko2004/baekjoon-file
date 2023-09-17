2b473793323be23e41cb5a666d5be5dfda6ca033
drko2004 <141055092+drko2004@users.noreply.github.com>
2023-09-18 05:29:32 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.py
@@ -1,8 +1,3 @@
 def solution(numbers):
-    answer = 0
-    ㅅㅂ_왜_이렇게_바꿔야함 = []
-    for i in numbers:
-        ㅅㅂ_왜_이렇게_바꿔야함.append(i)
-    ㅅㅂ_왜_이렇게_바꿔야함.sort()
-    answer = ㅅㅂ_왜_이렇게_바꿔야함[-1]*ㅅㅂ_왜_이렇게_바꿔야함[-2]
-    return answer
+    numbers.sort()
+    return numbers[-1]*numbers[-2]
