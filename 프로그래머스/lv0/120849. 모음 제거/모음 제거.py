18b4e50301e15e81ba2c82f9b5600030e7ea706e
drko2004 <141055092+drko2004@users.noreply.github.com>
2023-09-20 01:54:16 +0900

    [level 0] Title: 모음 제거, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    my_string = my_string.replace('a', '')
+    my_string = my_string.replace('e', '')
+    my_string = my_string.replace('i', '')
+    my_string = my_string.replace('o', '')
+    my_string = my_string.replace('u', '')
+    
+    return my_string
